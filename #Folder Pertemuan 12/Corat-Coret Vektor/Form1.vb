0056c98689e2fcde03dfa859f52a7bc4e2ea4a68
Ivan8Conan <ivan.conanta@ti.ukdw.ac.id>
2025-05-13 12:25:52 +0700

    #Folder Pertemuan 12

---
+++ #Folder Pertemuan 12/Corat-Coret Vektor/Form1.vb
@@ -0,0 +1,328 @@
+﻿Imports System.Drawing.Drawing2D
+
+Public Class frmUtama
+    Dim modegambar As String
+    Dim warnatepi As Color = Color.Black
+    Dim warnaisian As Color = Color.White
+    Dim tepi As New System.Drawing.Pen(warnatepi, 3)
+    Dim isian As New System.Drawing.SolidBrush(warnaisian)
+    Dim titik As Point = Nothing
+    Dim dipencet As Boolean = False
+    Dim bmp As Bitmap
+    Dim history As New Stack(Of Bitmap)
+
+
+    Private Sub frmUtama_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        btnBersihkan.PerformClick()
+        tepi.EndCap = LineCap.Round
+    End Sub
+
+    Private Sub RadioBebas_CheckedChanged(sender As Object, e As EventArgs) Handles RadioBebas.CheckedChanged
+        modegambar = "bebas"
+    End Sub
+
+    Private Sub RadioGaris_CheckedChanged(sender As Object, e As EventArgs) Handles RadioGaris.CheckedChanged
+        modegambar = "garis"
+    End Sub
+
+    Private Sub RadioKotak_CheckedChanged(sender As Object, e As EventArgs) Handles RadioKotak.CheckedChanged
+        modegambar = "kotak"
+    End Sub
+
+    Private Sub RadioElips_CheckedChanged(sender As Object, e As EventArgs) Handles RadioElips.CheckedChanged
+        modegambar = "elips"
+    End Sub
+
+    Private Sub RadioKotakIsi_CheckedChanged(sender As Object, e As EventArgs) Handles RadioKotakIsi.CheckedChanged
+        modegambar = "kotakisi"
+    End Sub
+
+    Private Sub RadioElipsIsi_CheckedChanged(sender As Object, e As EventArgs) Handles RadioElipsIsi.CheckedChanged
+        modegambar = "elipsisi"
+    End Sub
+
+    Private Sub NumericUpDown1_ValueChanged(sender As Object, e As EventArgs) Handles NumericUpDown1.ValueChanged
+        tepi.Width = NumericUpDown1.Value
+    End Sub
+
+    Private Sub PictureBox1_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles PictureBox1.MouseDown
+        SimpanKeHistory()
+        tepi.Color = warnatepi
+        isian.Color = warnaisian
+        titik = e.Location
+        dipencet = True
+    End Sub
+
+    Private Sub PictureBox1_MouseMove(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles PictureBox1.MouseMove
+        Select Case modegambar
+            Case "bebas"
+                If dipencet Then
+                    Using g As Graphics = Graphics.FromImage(PictureBox1.Image)
+                        g.DrawLine(tepi, titik, e.Location)
+                    End Using
+                    PictureBox1.Invalidate()
+                    TextBox1.AppendText("o " + "warnatepi" + " " + warnatepi.R.ToString + " " + warnatepi.G.ToString + " " + warnatepi.B.ToString & vbNewLine)
+                    TextBox1.AppendText("o garis " + titik.X.ToString + " " + titik.Y.ToString + " " + e.X.ToString + " " + e.Y.ToString & vbNewLine)
+                    titik = e.Location
+                End If
+        End Select
+    End Sub
+
+    Private Sub PictureBox1_MouseUp(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles PictureBox1.MouseUp
+        SimpanKeHistory()
+        Select Case modegambar
+            Case "garis"
+                Using g As Graphics = Graphics.FromImage(PictureBox1.Image)
+                    g.DrawLine(tepi, titik, e.Location)
+                End Using
+            Case "kotak"
+                Dim rect As New Rectangle(titik.X, titik.Y, e.X - titik.X, e.Y - titik.Y)
+                Using g As Graphics = Graphics.FromImage(PictureBox1.Image)
+                    g.DrawRectangle(tepi, rect)
+                End Using
+            Case "elips"
+                Dim rect As New Rectangle(titik.X, titik.Y, e.X - titik.X, e.Y - titik.Y)
+                Using g As Graphics = Graphics.FromImage(PictureBox1.Image)
+                    g.DrawEllipse(tepi, rect)
+                End Using
+            Case "kotakisi"
+                Dim rect As New Rectangle(titik.X, titik.Y, e.X - titik.X, e.Y - titik.Y)
+                Using g As Graphics = Graphics.FromImage(PictureBox1.Image)
+                    g.FillRectangle(isian, rect)
+                End Using
+            Case "elipsisi"
+                Dim rect As New Rectangle(titik.X, titik.Y, e.X - titik.X, e.Y - titik.Y)
+                Using g As Graphics = Graphics.FromImage(PictureBox1.Image)
+                    g.FillEllipse(isian, rect)
+                End Using
+        End Select
+        PictureBox1.Invalidate()
+        dipencet = False
+        dipencet = False
+        'If (titik.X <> e.X And titik.Y <> e.Y) Then
+        '    TextBox1.AppendText("o " + "warnatepi" + " " + warnatepi.R.ToString + " " + warnatepi.G.ToString + " " + warnatepi.B.ToString & vbNewLine)
+        '    TextBox1.AppendText("o " + "warnaisian" + " " + warnaisian.R.ToString + " " + warnaisian.G.ToString + " " + warnaisian.B.ToString & vbNewLine)
+        '    TextBox1.AppendText("o " + modegambar + " " + titik.X.ToString + " " + titik.Y.ToString + " " + e.X.ToString + " " + e.Y.ToString & vbNewLine)
+        'End If
+        If (titik.X <> e.X And titik.Y <> e.Y) Then
+            TextBox1.AppendText("o " + "warnatepi" + " " + warnatepi.R.ToString + " " + warnatepi.G.ToString + " " + warnatepi.B.ToString & vbNewLine)
+            TextBox1.AppendText("o " + "warnaisian" + " " + warnaisian.R.ToString + " " + warnaisian.G.ToString + " " + warnaisian.B.ToString & vbNewLine)
+            TextBox1.AppendText("o " + modegambar + " " + titik.X.ToString + " " + titik.Y.ToString + " " + e.X.ToString + " " + e.Y.ToString & vbNewLine)
+            TextBox1.AppendText("o " + "ukuran" + " " + tepi.Width.ToString() & vbNewLine) ' Menyimpan ukuran garis tepi
+        End If
+    End Sub
+
+    Private Sub Warna1_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Warna1.MouseDown
+        If e.Button <> MouseButtons.Right Then
+            warnatepi = Warna1.BackColor
+            ShapeTepi.BackColor = warnatepi
+        Else
+            warnaisian = Warna1.BackColor
+            ShapeIsian.BackColor = warnaisian
+        End If
+    End Sub
+
+    Private Sub Warna2_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Warna2.MouseDown
+        If e.Button <> MouseButtons.Right Then
+            warnatepi = Warna2.BackColor
+            ShapeTepi.BackColor = warnatepi
+        Else
+            warnaisian = Warna2.BackColor
+            ShapeIsian.BackColor = warnaisian
+        End If
+    End Sub
+
+    Private Sub Warna3_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Warna3.MouseDown
+        If e.Button <> MouseButtons.Right Then
+            warnatepi = Warna3.BackColor
+            ShapeTepi.BackColor = warnatepi
+        Else
+            warnaisian = Warna3.BackColor
+            ShapeIsian.BackColor = warnaisian
+        End If
+    End Sub
+
+    Private Sub Warna4_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Warna4.MouseDown
+        If e.Button <> MouseButtons.Right Then
+            warnatepi = Warna4.BackColor
+            ShapeTepi.BackColor = warnatepi
+        Else
+            warnaisian = Warna4.BackColor
+            ShapeIsian.BackColor = warnaisian
+        End If
+    End Sub
+
+    Private Sub Warna5_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Warna5.MouseDown
+        If e.Button <> MouseButtons.Right Then
+            warnatepi = Warna5.BackColor
+            ShapeTepi.BackColor = warnatepi
+        Else
+            warnaisian = Warna5.BackColor
+            ShapeIsian.BackColor = warnaisian
+        End If
+    End Sub
+
+    Private Sub Warna6_MouseDown(ByVal sender As Object, ByVal e As System.Windows.Forms.MouseEventArgs) Handles Warna6.MouseDown
+        If e.Button <> MouseButtons.Right Then
+            warnatepi = Warna6.BackColor
+            ShapeTepi.BackColor = warnatepi
+        Else
+            warnaisian = Warna6.BackColor
+            ShapeIsian.BackColor = warnaisian
+        End If
+    End Sub
+
+    Private Sub btnBersihkan_Click(sender As Object, e As EventArgs) Handles btnBersihkan.Click
+        bmp = New Bitmap(PictureBox1.Width, PictureBox1.Height)
+        Using g As Graphics = Graphics.FromImage(bmp)
+            g.Clear(Color.White)
+        End Using
+        PictureBox1.Image = bmp
+    End Sub
+
+    Private Sub btnGbrUlang_Click(sender As Object, e As EventArgs) Handles btnGbrUlang.Click
+        Dim a As Integer = TextBox1.Lines.Count
+        For i As Integer = 0 To a
+            Dim teksbaris As String = TextBox1.Lines(i)
+            Dim pecah() As String
+            pecah = teksbaris.Split(" "c)
+            On Error Resume Next
+            pecah(1) = pecah(1).Trim(" "c)
+            Select Case pecah(1)
+                Case "warnatepi"
+                    warnatepi = Color.FromArgb(CByte(pecah(2)), CByte(pecah(3)), CByte(pecah(4)))
+                    tepi.Color = warnatepi
+                Case "warnaisian"
+                    warnaisian = Color.FromArgb(CByte(pecah(2)), CByte(pecah(3)), CByte(pecah(4)))
+                    isian.Color = warnaisian
+                Case "garis"
+                    Using g As Graphics = Graphics.FromImage(PictureBox1.Image)
+                        g.DrawLine(tepi, CInt(pecah(2)), CInt(pecah(3)), CInt(pecah(4)), CInt(pecah(5)))
+                    End Using
+                Case "kotak"
+                    Dim rect As New Rectangle(CInt(pecah(2)), CInt(pecah(3)), CInt(pecah(4)) - CInt(pecah(2)), CInt(pecah(5)) - CInt(pecah(3)))
+                    Using g As Graphics = Graphics.FromImage(PictureBox1.Image)
+                        g.DrawRectangle(tepi, rect)
+                    End Using
+                Case "elips"
+                    Dim rect As New Rectangle(CInt(pecah(2)), CInt(pecah(3)), CInt(pecah(4)) - CInt(pecah(2)), CInt(pecah(5)) - CInt(pecah(3)))
+                    Using g As Graphics = Graphics.FromImage(PictureBox1.Image)
+                        g.DrawEllipse(tepi, rect)
+                    End Using
+                Case "kotakisi"
+                    Dim rect As New Rectangle(CInt(pecah(2)), CInt(pecah(3)), CInt(pecah(4)) - CInt(pecah(2)), CInt(pecah(5)) - CInt(pecah(3)))
+                    Using g As Graphics = Graphics.FromImage(PictureBox1.Image)
+                        g.FillRectangle(isian, rect)
+                    End Using
+                Case "elipsisi"
+                    Dim rect As New Rectangle(CInt(pecah(2)), CInt(pecah(3)), CInt(pecah(4)) - CInt(pecah(2)), CInt(pecah(5)) - CInt(pecah(3)))
+                    Using g As Graphics = Graphics.FromImage(PictureBox1.Image)
+                        g.FillEllipse(isian, rect)
+                    End Using
+            End Select
+        Next
+        PictureBox1.Invalidate()
+    End Sub
+
+    Private Sub KeluarToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles KeluarToolStripMenuItem.Click
+        Application.Exit()
+    End Sub
+
+    Private Sub SimpanToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SimpanToolStripMenuItem.Click
+        Dim simpanDialog As New SaveFileDialog()
+        simpanDialog.Filter = "PNG Image|*.png|JPEG Image|*.jpg|Bitmap Image|*.bmp"
+        simpanDialog.Title = "Simpan Gambar"
+
+        If simpanDialog.ShowDialog() = DialogResult.OK Then
+            Dim path As String = simpanDialog.FileName
+            Try
+                Dim format As Imaging.ImageFormat = Imaging.ImageFormat.Png
+                Select Case System.IO.Path.GetExtension(path).ToLower()
+                    Case ".jpg", ".jpeg"
+                        format = Imaging.ImageFormat.Jpeg
+                    Case ".bmp"
+                        format = Imaging.ImageFormat.Bmp
+                End Select
+                PictureBox1.Image.Save(path, format)
+                MessageBox.Show("Gambar berhasil disimpan!", "Sukses", MessageBoxButtons.OK, MessageBoxIcon.Information)
+            Catch ex As Exception
+                MessageBox.Show("Terjadi kesalahan saat menyimpan gambar: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            End Try
+        End If
+    End Sub
+
+    Private Sub SimpanKeHistory()
+        If PictureBox1.Image IsNot Nothing Then
+            history.Push(New Bitmap(PictureBox1.Image))
+        End If
+    End Sub
+
+    Private Sub btnUndo_Click(sender As Object, e As EventArgs) Handles btnUndo.Click
+        If history.Count > 0 Then
+            If TextBox1.Lines.Length > 0 Then
+                Dim lastLineCount As Integer = 1
+                Dim lastLine As String = TextBox1.Lines(TextBox1.Lines.Length - 1)
+
+                If lastLine.StartsWith("o ") Then
+                    Dim parts() As String = lastLine.Split(" "c)
+                    If parts.Length > 1 AndAlso parts(1) = "ukuran" Then
+                        lastLineCount = 1
+                    End If
+                End If
+
+                For i As Integer = 0 To lastLineCount - 1
+                    If TextBox1.Lines.Length > 0 Then
+                        TextBox1.Lines = TextBox1.Lines.Take(TextBox1.Lines.Length - 1).ToArray()
+                    End If
+                Next
+            End If
+
+            Dim a As Integer = TextBox1.Lines.Count
+            btnBersihkan.PerformClick()
+            For i As Integer = 0 To a - 1
+                Dim teksbaris As String = TextBox1.Lines(i)
+                Dim pecah() As String
+                pecah = teksbaris.Split(" "c)
+                On Error Resume Next
+                pecah(1) = pecah(1).Trim(" "c)
+                Select Case pecah(1)
+                    Case "warnatepi"
+                        warnatepi = Color.FromArgb(CByte(pecah(2)), CByte(pecah(3)), CByte(pecah(4)))
+                        tepi.Color = warnatepi
+                    Case "warnaisian"
+                        warnaisian = Color.FromArgb(CByte(pecah(2)), CByte(pecah(3)), CByte(pecah(4)))
+                        isian.Color = warnaisian
+                    Case "garis"
+                        Using g As Graphics = Graphics.FromImage(PictureBox1.Image)
+                            g.DrawLine(tepi, CInt(pecah(2)), CInt(pecah(3)), CInt(pecah(4)), CInt(pecah(5)))
+                        End Using
+                    Case "kotak"
+                        Dim rect As New Rectangle(CInt(pecah(2)), CInt(pecah(3)), CInt(pecah(4)) - CInt(pecah(2)), CInt(pecah(5)) - CInt(pecah(3)))
+                        Using g As Graphics = Graphics.FromImage(PictureBox1.Image)
+                            g.DrawRectangle(tepi, rect)
+                        End Using
+                    Case "elips"
+                        Dim rect As New Rectangle(CInt(pecah(2)), CInt(pecah(3)), CInt(pecah(4)) - CInt(pecah(2)), CInt(pecah(5)) - CInt(pecah(3)))
+                        Using g As Graphics = Graphics.FromImage(PictureBox1.Image)
+                            g.DrawEllipse(tepi, rect)
+                        End Using
+                    Case "kotakisi"
+                        Dim rect As New Rectangle(CInt(pecah(2)), CInt(pecah(3)), CInt(pecah(4)) - CInt(pecah(2)), CInt(pecah(5)) - CInt(pecah(3)))
+                        Using g As Graphics = Graphics.FromImage(PictureBox1.Image)
+                            g.FillRectangle(isian, rect)
+                        End Using
+                    Case "elipsisi"
+                        Dim rect As New Rectangle(CInt(pecah(2)), CInt(pecah(3)), CInt(pecah(4)) - CInt(pecah(2)), CInt(pecah(5)) - CInt(pecah(3)))
+                        Using g As Graphics = Graphics.FromImage(PictureBox1.Image)
+                            g.FillEllipse(isian, rect)
+                        End Using
+                    Case "ukuran"
+                        tepi.Width = CInt(pecah(2))
+                End Select
+            Next
+            PictureBox1.Invalidate()
+        Else
+            MessageBox.Show("Tidak ada aksi yang bisa di-undo.", "Undo", MessageBoxButtons.OK, MessageBoxIcon.Information)
+        End If
+    End Sub
+End Class
